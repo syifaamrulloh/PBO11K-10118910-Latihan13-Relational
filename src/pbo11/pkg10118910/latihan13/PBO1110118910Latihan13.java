6d4b1e83f37ea4740f53da5ea2ecc01b7bdaadee
ADMIN <ADMIN@ADMIN-PC>
2019-09-20 23:43:13 +0700

    Latihan-Java

---
+++ src/pbo11/pkg10118910/latihan13/PBO1110118910Latihan13.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11.pkg10118910.latihan13;
+
+/**
+ *
+ * @author
+ * NAMA  : Muhamad Syifa Amrulloh 
+ * KELAS : PBO11K 
+ * NIM   : 10118910
+ *
+ * Description Program : Program ini Berisi pengunaan Relational
+ */
+public class PBO1110118910Latihan13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("a >= b = " + (bil1 >= bil2));
+        System.out.println("a <= b = " + (bil1 <= bil2));
+    }
+    
+}
